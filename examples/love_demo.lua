52cfed92e302c225d0831f06b7b178f93de48edc
kari <mr.kari.ikonen@gmail.com>
2023-11-18 19:31:33 +0200

    FIX dump data

--- examples/love_demo.lua
+++ examples/love_demo.lua
@@ -8,5 +8,16 @@ mi = require("moonimage")
 data, w, h, channels = mi.load("sunflowers.jpg", 'rgba')
 
 print(w, h, channels)
+print("-----------")
+for y=0, 10 do
+  for x=0, 10 do
+    local i = y * 10 + x + 1
+    local b = string.byte(data, i)
+    printf(" %0x", b)
+  end
+  printf("\n")
+end
+print("-----------")
+
 -- Write it to a bmp file:
 mi.write_bmp("output.bmp", w, h, channels, data)
--- examples/main.lua
+++ examples/main.lua
@@ -2,6 +2,8 @@
 table.pack = pack
 table.unpack = unpack
 
+function printf(...) io.write(string.format(...)) end
+
 function love.load()
    require 'love_demo'
    os.exit()
